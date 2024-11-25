cac7f9428f453364a17fd53d01160c4a2b9422ed
Agustín Alfonso González Iríbar <agustin.gonzalez112@alu.ulpgc.es>
2024-11-25 18:00:48 +0000

    Realizado inicio de renderizado con three.js

--- public/animacion.js
+++ public/animacion.js
@@ -1,1 +1,25 @@
-console.log("hola")
+import * as THREE from "three";
+
+let escena, camara, renderer
+
+init()
+animationLoop()
+
+function init() {
+    escena = new THREE.Scene();
+
+    camara = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
+
+function animationLoop() {
+    renderer.render(escena, camara);
+}
