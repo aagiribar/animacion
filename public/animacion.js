383c8d4313958d2b00efb00282ea3ad1162dfcef
Agustín Alfonso González Iríbar <agustin.gonzalez112@alu.ulpgc.es>
2024-12-02 17:55:54 +0000

    Añadida librería ammo.js e inicializado el mundo físico

--- public/animacion.js
+++ public/animacion.js
@@ -1,11 +1,28 @@
 import * as THREE from "three";
 
-let escena, camara, renderer
+let escena, camara, renderer;
+let textureLoader;
+
+// Mundo físico con Ammo
+let physicsWorld;
+const gravityConstant = 7.8;
+let collisionConfiguration;
+let dispatcher;
+let broadphase;
+let solver;
+const margin = 0.05
 
 init()
 animationLoop()
 
 function init() {
+    // Elementos gráficos
+    initGraphics();
+    // Elementos del mundo físico
+    initPhysics();
+}
+
+function initGraphics() {
     escena = new THREE.Scene();
 
     camara = new THREE.PerspectiveCamera(
@@ -18,6 +35,29 @@ function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+
+    textureLoader = new THREE.TextureLoader();
+}
+
+function initPhysics() {
+    // Colisiones
+    collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
+    // Gestor de colisiones convexas y cóncavas
+    dispatcher = new Ammo.btDbvtBroadphase();
+    // Resuelve restricciones de reglas físicas fuerzas, gravedad, etc.
+    solver = new Ammo.btSequentialImpulseConstraintSolver();
+    // Crea en mundo físico
+    physicsWorld = new Ammo.btDiscreteDynamicsWorld(
+        dispatcher,
+        broadphase,
+        solver,
+        collisionConfiguration
+    );
+    // Establece gravedad
+    physicsWorld.setGravity(new Ammo.btVector3(0, -gravityConstant, 0));
+
+    transformAux1 = new Ammo.btTransform();
+    tempBtVec3_1 = new Ammo.btVector3(0, 0, 0);
 }
 
 function animationLoop() {
