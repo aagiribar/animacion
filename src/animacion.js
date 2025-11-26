5c5ce567033ccfc7419e8a63525cd77557d6f8bf
Agustín Alfonso González Iríbar <aagiribar@yahoo.es>
2025-11-26 14:49:30 +0000

    Implementada creación de objetos de la simulación en el módulo simObjects.js

--- src/animacion.js
+++ src/animacion.js
@@ -1,12 +1,6 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { initGUI, UIelements, gameFolder, placeSelector, endButton } from "/modules/gui.js";
-
-// Elementos de la simulación
-let scene, camera, renderer;
-let textureLoader;
-let controls;
-const clock = new THREE.Clock();
+import { initGraphics, scene, camera, renderer, textureLoader, clock } from "/modules/simObjects.js"
 
 // Mundo físico con Ammo
 let physicsWorld;
@@ -76,61 +70,6 @@ function init() {
     initInfo();
     // Interfaz de usuario
     initGUI();
-}
-
-// Función que inicializa los elementos gráficos
-function initGraphics() {
-    // Creación de la escena
-    scene = new THREE.Scene();
-
-    // Creación de la cámara
-    camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-    );
-
-    scene.background = new THREE.Color(0xbfd1e5);
-    camera.position.set(-14, 8, 16);
-
-    // Creación del renderer
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true;
-    document.body.appendChild(renderer.domElement);
-
-    // TextureLoader para cargar texturas
-    textureLoader = new THREE.TextureLoader();
-
-    // Creación de controles de tipo orbital
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(0, 2, 0);
-    controls.update();
-
-    //Luces
-    const ambientLight = new THREE.AmbientLight(0x707070);
-    scene.add(ambientLight);
-
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(-10, 18, 5);
-    light.castShadow = true;
-    const d = 14;
-    light.shadow.camera.left = -d;
-    light.shadow.camera.right = d;
-    light.shadow.camera.top = d;
-    light.shadow.camera.bottom = -d;
-
-    light.shadow.camera.near = 2;
-    light.shadow.camera.far = 50;
-
-    light.shadow.mapSize.x = 1024;
-    light.shadow.mapSize.y = 1024;
-
-    scene.add(light);
-
-    //Redimensión de la ventana
-    window.addEventListener("resize", onWindowResize);
 }
 
 // Función que inicializa las físicas de la simulación
@@ -389,16 +328,6 @@ function createRigidBody(object, physicsShape, mass, pos, quat, vel, angVel) {
     physicsWorld.addRigidBody(body);
 
     return body;
-}
-
-// Función a la que se llama cuando ocurre una redimensión de la ventana
-function onWindowResize() {
-    // Se modifica la relación de aspecto de la camara
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-  
-    // Se actualiza el tamaño del render
-    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 // Función que actualiza los contadores de objetos sobre la plataforma
---
+++ src/modules/simObjects.js
@@ -0,0 +1,73 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// Elementos de la simulación
+export let scene, camera, renderer;
+export let textureLoader;
+let controls;
+export const clock = new THREE.Clock();
+
+// Función que inicializa los elementos gráficos
+export function initGraphics() {
+    // Creación de la escena
+    scene = new THREE.Scene();
+
+    // Creación de la cámara
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+
+    scene.background = new THREE.Color(0xbfd1e5);
+    camera.position.set(-14, 8, 16);
+
+    // Creación del renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    document.body.appendChild(renderer.domElement);
+
+    // TextureLoader para cargar texturas
+    textureLoader = new THREE.TextureLoader();
+
+    // Creación de controles de tipo orbital
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 2, 0);
+    controls.update();
+
+    //Luces
+    const ambientLight = new THREE.AmbientLight(0x707070);
+    scene.add(ambientLight);
+
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(-10, 18, 5);
+    light.castShadow = true;
+    const d = 14;
+    light.shadow.camera.left = -d;
+    light.shadow.camera.right = d;
+    light.shadow.camera.top = d;
+    light.shadow.camera.bottom = -d;
+
+    light.shadow.camera.near = 2;
+    light.shadow.camera.far = 50;
+
+    light.shadow.mapSize.x = 1024;
+    light.shadow.mapSize.y = 1024;
+
+    scene.add(light);
+
+    //Redimensión de la ventana
+    window.addEventListener("resize", onWindowResize);
+}
+
+// Función a la que se llama cuando ocurre una redimensión de la ventana
+function onWindowResize() {
+    // Se modifica la relación de aspecto de la camara
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    // Se actualiza el tamaño del render
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
